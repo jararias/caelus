8a72e08b27e79e91b114c15949accd594666fb23
Jose A Ruiz Arias <jose.ruiz-arias@solargis.com>
2023-05-24 22:00:50 +0200

    Added REQUIRED_TO_CLASSIFY

--- caelus/__init__.py
+++ caelus/__init__.py
@@ -7,3 +7,5 @@ from .version import version as __version__
 
 from . import data, diagnostics
 from .classifier import classify
+
+REQUIRED_TO_CLASSIFY = ['longitude', 'sza', 'eth', 'ghi', 'ghics', 'ghicda']
