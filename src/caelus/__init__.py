a507da8684365e759511676dddd619f97fcfa504
Jose A Ruiz-Arias <94106583+jararias@users.noreply.github.com>
2025-02-02 20:32:13 +0100

    update version to 0.2.0

--- src/caelus/__init__.py
+++ src/caelus/__init__.py
@@ -6,6 +6,6 @@ from loguru import logger
 from . import data, diagnostics
 from .classifier import classify
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 REQUIRED_TO_CLASSIFY = {"longitude", "sza", "eth", "ghi", "ghics", "ghicda"}
